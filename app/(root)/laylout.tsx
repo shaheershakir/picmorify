690fa01172ee87d0c9608b91906e834251738068
Shaheer Shakir <shaheershakir22@gmail.com>
2024-03-03 21:09:18 +0530

    chander Transformation route name header

--- app/(root)/laylout.tsx
+++ app/(root)/laylout.tsx
@@ -7,6 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main className='root'>
       <SideBar />
       <MobileNav />
+
       <div className='root-container'>
         <div className='wrapper'>{children}</div>
       </div>
--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,12 @@
+import Header from '@/components/shared/Header';
 import React from 'react';
+import { transformationTypes } from '@/constants';
 
-const AddTransformationType = () => {
-  return <div>AddTransformationType</div>;
+const AddTransformationType = ({ params: { type } }: SearchParamProps) => {
+  const transformation = transformationTypes[type];
+  return (
+    <Header title={transformation.title} subtitle={transformation.subTitle} />
+  );
 };
 
 export default AddTransformationType;
